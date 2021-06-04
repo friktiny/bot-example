585412582a37d84cf37705ef3dcdd369adc42fb6
Dotwood Media <55753961+DotwoodMedia@users.noreply.github.com>
2021-06-04 16:10:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Dotwood = require("dotwood.js");
 
 let client = new Dotwood.Client({
-    token: "Nzk5MDI3MzE0NzQ1OTMzODk0.X_9mLQ.SCIPDinAqhyMowEBWC3G23NTDGo", // Your bot toke
+    token: "TOKEN", // Your bot toke
 })
 
 client.login();
